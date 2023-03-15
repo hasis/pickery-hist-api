e5d727eed81005dabef8faf0ed82d63bd6266efe
nikita hasis <nikitahasis@gmail.com>
2023-03-16 00:40:32 +0100

    Added leaderboard POST and getMatches get.

--- TODO.md
+++ TODO.md
@@ -1,9 +1,9 @@
 #### Pickery 3.0
 
 - [ ] Create New Game / Pack (Needs UI)
-- [ ] Create new Leaderboard Entry
-- [ ] Return Random Fixtures
-- [ ] Import from Football Data
+- [X] Create new Leaderboard Entry
+- [ ] Return Random Fixtures (currently returning in order)
+- [-] Import from Football Data
 - [ ] Import NBA
 - [ ] Import NFL
-- [ ] Swagger
+- [X] Swagger
--- routes/root.js
+++ routes/root.js
@@ -41,6 +41,90 @@ module.exports = async function (fastify, opts) {
           return data;
         }
       },
+    }),
+    fastify.route({
+      url: "/leaderboard",
+      method: ["POST"],
+      schema: {
+        body: PostLeaderboardBody,
+        summary: "New Leaderboard Entry",
+        description: "Adds to the leaderboard",
+        tags: ["Leaderboard"],
+      },
+      handler: async (request, reply) => {
+        console.log(request.body);
+
+        const { supabase } = fastify;
+        const { data, error } = await supabase
+          .from("leaderboard")
+          .insert(request.body)
+          .select();
+
+        if (error) {
+          return error;
+        } else {
+          console.log(data);
+          return data;
+        }
+      },
+    }),
+    fastify.route({
+      url: "/getMatches",
+      method: ["GET"],
+      schema: {
+        summary: "Get More Matches",
+        description: "Fetches more matches from the ether",
+        tags: ["Feed"],
+      },
+      handler: async (request, reply) => {
+        try {
+          const response = await fetch(
+            "https://api.football-data.org/v4/matches",
+            {
+              headers: {
+                "X-Auth-Token": "7457c0bb6b7f4c08aff02df8d60d6b72",
+              },
+            }
+          );
+          const apiResponse = await response.json();
+          const matches = apiResponse.matches.map((match) => {
+            return {
+              id: match.id,
+              homeTeamName: match.homeTeam.name,
+              homeTeamShortName: match.homeTeam.shortName,
+              awayTeamName: match.awayTeam.name,
+              awayTeamShortName: match.awayTeam.shortName,
+              winner: match.score.winner,
+              gameDate: match.utcDate,
+            };
+          });
+
+          const { supabase } = fastify;
+          const { data, error } = await supabase
+            .from("games")
+            .insert(matches)
+            .select();
+
+          if (error) {
+            return error;
+          } else {
+            return data;
+          }
+          return matches;
+        } catch (err) {
+          console.error(err);
+          reply.status(500).send("Error fetching matches");
+        }
+      },
     })
   );
+};
+
+
+const PostLeaderboardBody = {
+  type: "object",
+  properties: {
+    username: { type: "string", default: "abc" },
+    score: { type: "integer", minimum: 0 },
+  },
 };
