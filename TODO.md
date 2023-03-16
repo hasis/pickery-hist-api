ce56ff13857ccaa38ac002717959e45a38015970
nikita hasis <nikitahasis@gmail.com>
2023-03-16 23:39:46 +0100

    Returning Random Feed

--- TODO.md
+++ TODO.md
@@ -2,8 +2,11 @@
 
 - [ ] Create New Game / Pack (Needs UI)
 - [X] Create new Leaderboard Entry
-- [ ] Return Random Fixtures (currently returning in order)
+- [X] Return Random Fixtures (currently returning in order)
 - [-] Import from Football Data
-- [ ] Import NBA
-- [ ] Import NFL
+- [ ] Import NBA 
+    https://www.thesportsdb.com/api/v1/json/3/eventsseason.php?id=4387&s=2021-2022
+- [X] Import NFL
+- [ ] Import MLB
+- [ ] Import Tennis
 - [X] Swagger
--- routes/root.js
+++ routes/root.js
@@ -1,14 +1,5 @@
 "use strict";
 
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-}
-
 module.exports = async function (fastify, opts) {
   fastify.route(
     {
@@ -21,7 +12,7 @@ module.exports = async function (fastify, opts) {
       },
       handler: async (request, reply) => {
         const { supabase } = fastify;
-        const { data, error } = await supabase.from("games").select();
+        const { data, error } = await supabase.rpc("randomfeed");
 
         if (error) {
           return error;
@@ -83,7 +74,6 @@ module.exports = async function (fastify, opts) {
         if (error) {
           return error;
         } else {
-          console.log(data);
           return data;
         }
       },
@@ -99,7 +89,7 @@ module.exports = async function (fastify, opts) {
       handler: async (request, reply) => {
         try {
           const response = await fetch(
-            "https://api.football-data.org/v4/competitions/2001/matches?season=2020",
+            "https://api.football-data.org/v4/competitions/2021/matches?season=2020",
             {
               headers: {
                 "X-Auth-Token": "7457c0bb6b7f4c08aff02df8d60d6b72",
@@ -115,7 +105,111 @@ module.exports = async function (fastify, opts) {
               awayTeamName: match.awayTeam.name,
               awayTeamShortName: match.awayTeam.shortName,
               winner: match.score.winner,
+              homeTeamScore: match.score.fullTime.home,
+              awayTeamScore: match.score.fullTime.away,
               gameDate: match.utcDate,
+            };
+          });
+
+          const { supabase } = fastify;
+          const { data, error } = await supabase
+            .from("games")
+            .insert(matches)
+            .select();
+
+          if (error) {
+            return error;
+          } else {
+            return data;
+          }
+          return matches;
+        } catch (err) {
+          console.error(err);
+          reply.status(500).send("Error fetching matches");
+        }
+      },
+    }),
+    fastify.route({
+      url: "/getNFLMatches",
+      method: ["GET"],
+      schema: {
+        summary: "Get More Matches",
+        description: "Fetches more matches from the ether",
+        tags: ["Feed"],
+      },
+      handler: async (request, reply) => {
+        try {
+          const response = await fetch(
+            "https://www.thesportsdb.com/api/v1/json/3/eventsseason.php?id=4391&s=2006"
+          );
+          const apiResponse = await response.json();
+
+          const matches = apiResponse.events.map((match) => {
+            return {
+              id: match.idEvent,
+              homeTeamName: match.strHomeTeam,
+              homeTeamShortName: match.strHomeTeam.split(" ").pop(),
+              awayTeamName: match.strAwayTeam,
+              awayTeamShortName: match.strAwayTeam.split(" ").pop(),
+              winner:
+                match.intHomeScore > match.intAwayScore
+                  ? "HOME_TEAM"
+                  : "AWAY_TEAM",
+              homeTeamScore: match.intHomeScore,
+              awayTeamScore: match.intAwayScore,
+              gameDate: match.dateEvent,
+              eventType: "football",
+            };
+          });
+
+          const { supabase } = fastify;
+          const { data, error } = await supabase
+            .from("games")
+            .insert(matches)
+            .select();
+
+          if (error) {
+            return error;
+          } else {
+            return data;
+          }
+          return matches;
+        } catch (err) {
+          console.error(err);
+          reply.status(500).send("Error fetching matches");
+        }
+      },
+    }),
+    fastify.route({
+      url: "/getMLSMatches",
+      method: ["GET"],
+      schema: {
+        summary: "Get More Matches",
+        description: "Fetches more matches from the ether",
+        tags: ["Feed"],
+      },
+      handler: async (request, reply) => {
+        try {
+          const response = await fetch(
+            "https://www.thesportsdb.com/api/v1/json/3/eventsseason.php?id=4346&s=2021"
+          );
+          const apiResponse = await response.json();
+
+          const matches = apiResponse.events.map((match) => {
+            return {
+              id: match.idEvent,
+              homeTeamName: match.strHomeTeam,
+              homeTeamShortName: match.strHomeTeam,
+              awayTeamName: match.strAwayTeam,
+              awayTeamShortName: match.strAwayTeam,
+              winner:
+                match.intHomeScore > match.intAwayScore
+                  ? "HOME_TEAM"
+                  : "AWAY_TEAM",
+              homeTeamScore: match.intHomeScore,
+              awayTeamScore: match.intAwayScore,
+              gameDate: match.dateEvent,
+              eventType: "soccer",
             };
           });
 
